2c4a1ee69a976ee0bf4b243033368b2181bcdd9d
OzanTemizel <temizelozan@gmail.com>
2022-08-16 16:45:45 +0300

    Initial commit

---
+++ src/Deneme.java
@@ -0,0 +1,19 @@
+public class Deneme {
+    public static void main(String[] args) {
+
+        int counter = 0;
+        for(int i=2;i<=100;i++) {
+            int check = 0;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    check = 1;
+                    break;
+                }
+            }
+            if(check==0) {
+                System.out.print(i+",");
+                counter++;
+            }
+        }
+    }
+}
